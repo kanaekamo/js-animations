9bb0953fc80d990ccc723725275077e3b2e72f74
kanaekamo <sugiura.kamo@gmail.com>
2024-06-16 18:36:11 +0900

    課題提出

---
+++ scroll-anime/work/js/app.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded",function(){
+  const observer = new IntersectionObserver((entrise)=>{
+    entrise.forEach((entry)=>{
+      if (entry.isIntersecting){
+        entry.target.classList.add("displayed");
+      }else{
+        entry.target.classList.remove("displayed");
+      }
+    });
+  },{threshold:1,})
+  document.querySelectorAll(".box").forEach((box)=>{
+    observer.observe(box);
+  });
+});
